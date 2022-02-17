793ad41f84f9a8e27d0624a840d75ac895aa3330
kin <ymist17@gmail.com>
2022-02-17 11:39:59 +0900

    product url 수정5

--- src/Content/MergeXlsx/MergeXlsx.js
+++ src/Content/MergeXlsx/MergeXlsx.js
@@ -11,7 +11,7 @@ const MergeXlsx = () => {
     const sendFileEvent = () => {
         const formData = new FormData();
         files.map(file => formData.append('files', file))
-        axios.post("http://localhost:8000/api/file/xlsxtopdf", formData, {
+        axios.post("http://127.0.0.1:8000/api/file/xlsxtopdf", formData, {
             headers: {
                 'Content-Disposition': "attachment; filename=union.zip",
                 "Content-Type" : "multipart/form-data",
